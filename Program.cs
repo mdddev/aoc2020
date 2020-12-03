1f81bebe3de5e131536853856a3ca03c83c7959b
mdddev <33447232+mdddev@users.noreply.github.com>
2020-12-03 11:03:03 +0100

    meh

--- Program.cs
+++ Program.cs
@@ -14,10 +14,8 @@ namespace AdventOfCode2020
         private static void _solveDay1()
         {
             var day1 = new Day1();
-            var resultOfDay1Part1 = day1.Part1();
-            var resultOfDay1Part2 = day1.Part2();
-            Console.WriteLine($"Result of day1-1= {resultOfDay1Part1}");
-            Console.WriteLine($"Result of day1-2= {resultOfDay1Part2}");
+            Console.WriteLine($"Result of day1-1= {day1.Part1()}");
+            Console.WriteLine($"Result of day1-2= {day1.Part2()}");
         }
     }
 }
