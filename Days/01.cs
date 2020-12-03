ab8496012bc59a4aa0f6828b58bfdc223de225e0
mdddev <33447232+mdddev@users.noreply.github.com>
2020-12-03 10:35:59 +0100

    day1

---
+++ Days/01.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    public class Day1
+    {
+        private readonly int _target = 2020;
+        private readonly int[] _numbers = Inputs.Day1;
+
+        public int Part1()
+        {
+            // nested for loop to work with pairs
+            for (int i = 0; i < _numbers.Length; i++)
+            {
+                for (int j = 0; j < _numbers.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue; // ignore if same index
+                    }
+                    else
+                    {
+                        var n1 = _numbers[i];
+                        var n2 = _numbers[j];
+                        var sumOfBothNumbers =  n1 + n2;
+                        if (sumOfBothNumbers == _target)
+                        {
+                            return n1 * n2;
+                        }
+                    }
+                }
+            }
+
+            // if code reaches here
+            throw new Exception($"Could not find two numbers that add to {_target}");
+        }
+
+        // not dry code :/
+        public int Part2()
+        {
+            // nested for loop to work with pairs
+            for (int i = 0; i < _numbers.Length; i++)
+            {
+                for (int j = 0; j < _numbers.Length; j++)
+                {
+                    for (int k = 0; k < _numbers.Length; k++)
+                    {
+                        if (i == j && j == k)
+                        {
+                            continue; // ignore if same index
+                        }
+                        else
+                        {
+                            var n1 = _numbers[i];
+                            var n2 = _numbers[j];
+                            var n3 = _numbers[k];
+                            var sumOfNumbers =  n1 + n2 + n3;
+                            if (sumOfNumbers == _target)
+                            {
+                                return n1 * n2 * n3;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // if code reaches here
+            throw new Exception($"Could not find three numbers that add to {_target}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var resultOfDay1Part1 = new Day1().Part1();
+            Console.WriteLine($"Result of day1-1= {resultOfDay1Part1}");
+
+            var resultOfDay1Part2 = new Day1().Part2();
+            Console.WriteLine($"Result of day1-2= {resultOfDay1Part2}");
+
+        }
+    }
+}
