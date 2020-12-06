984a79efc3a1937a311351c1cdba9c7fb0818940
mdddev <33447232+mdddev@users.noreply.github.com>
2020-12-06 21:31:57 +0100

    day 4.2

--- Days/day04.cs
+++ Days/day04.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -14,23 +15,23 @@ namespace AdventOfCode2020.Days
         {
             var path = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Resources", "day04.txt");
             var data = (await File.ReadAllTextAsync(path, Encoding.UTF8));
-
-            _part1(data);
-            // _part2(data);
-        }
-
-        private static void _part1(string data)
-        {
             var items = _passportsData(data);
             var passports = items.Select(s => new Passport(s));
-            var validPassports = passports.Where(w => w.IsValid()).Count();
-            System.Console.WriteLine($"Result of day4-1= There are {validPassports} valid passports");
+
+            _part1(passports);
+            _part2(passports);
         }
 
-        private static void _part2(IEnumerable<string> data)
+        private static void _part1(IEnumerable<Passport> passports)
         {
+            var passportsWithRequiredFields = passports.Where(w => w.HasAllRequiredFields()).Count();
+            System.Console.WriteLine($"Result of day4-1= There are {passportsWithRequiredFields} valid passports (all required fields)");
+        }
 
-            System.Console.WriteLine($"Result of day4-2= ");
+        private static void _part2(IEnumerable<Passport> passports)
+        {
+            var validPassports = passports.Where(w => w.IsValid()).Count();
+            System.Console.WriteLine($"Result of day4-2= There are {validPassports} valid passports");
         }
 
         private static List<Dictionary<string, string>> _passportsData(string data)
@@ -79,7 +80,7 @@ namespace AdventOfCode2020.Days
             }
         }
 
-        public bool IsValid()
+        public bool HasAllRequiredFields()
         {
             PropertyInfo[] props = typeof(Passport).GetProperties();
             IEnumerable<string?> values = props.Select(s => (string?)s.GetValue(this));
@@ -95,6 +96,62 @@ namespace AdventOfCode2020.Days
             else
             {
                 return false;
+            }
+        }
+
+        public bool IsValid()
+        {
+            if (!HasAllRequiredFields())
+            {
+                return false;
+            }
+            else
+            {
+                bool validYear(string year, int min, int max)
+                {
+                    if (year.Length != 4) return false;
+                    var yearInt = Convert.ToInt32(year);
+                    if (yearInt < min || yearInt > max) return false;
+                    return true;
+                }
+
+                // byr, iyr, eyr
+                if (!validYear(Byr, 1920, 2002)) return false;
+                if (!validYear(Iyr, 2010, 2020)) return false;
+                if (!validYear(Eyr, 2020, 2030)) return false;
+
+                // hgt
+                var unit = Hgt.Substring(Hgt.Length - 2).ToLower();
+                if (unit != "cm" && unit != "in") return false;
+                var height = Convert.ToInt32(Hgt.Substring(0, Hgt.Length - 2));
+                if (unit == "cm")
+                {
+                    if (height < 150 || height >193) return false;
+                }
+                else
+                {
+                    if (height < 59 || height >76) return false;
+                }
+
+                // hcl
+                if (Hcl.Length != 7) return false;
+                try
+                {
+                    var colour = ColorTranslator.FromHtml(Hcl);
+                }
+                catch (System.Exception) // HEX not parseable
+                {
+                    return false;
+                }
+
+                // ecl
+                var validColours = new [] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+                if (!validColours.Contains(Ecl.ToLower())) return false;
+                
+                // pid
+                if (Pid.Length != 9) return false;
+
+                return true;
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AdventOfCode2020.Days;
 using System.Linq;
+using System.Drawing;
 
 namespace AdventOfCode2020
 {
